d557bd7eb3e4b8ad3b6f7945c0658d1e7a1d0674
xuan <lixx66@hotmail.com>
2022-04-11 13:04:29 +0800

    2.2

--- idc1/c/crtsurfdata2.cpp
+++ idc1/c/crtsurfdata2.cpp
@@ -8,8 +8,8 @@ int main(int argc,char *argv[]){
 //inifile气象参数文件 outpath logfile
 
   if(argc!=4){
-	printf("Using:./crtsurfdata1 inifile putpath logfile\n");
-	printf("Example:project/idc1/bin/crtsurfdata1 /project/idc1/ini/stcode.ini /tmp/surfdata /log/idc1/crtsurfdata1.log\n\n");
+	printf("Using:./crtsurfdata2 inifile putpath logfile\n");
+	printf("Example:project/idc2/bin/crtsurfdata2 /project/idc2/ini/stcode.ini /tmp/surfdata /log/idc2/crtsurfdata2.log\n\n");
 
 	printf("inifile 全国气象站点参数文件名。\n");
 	printf("outpath 全国气象站点数据文件存放目录。\n");
@@ -22,13 +22,13 @@ int main(int argc,char *argv[]){
   	printf("logfile.Open(%s) failed.\n,argv[3]"); return -1;
   }
 
-  logfile.Write("crtsurfdata1 开始运行。\n");
+  logfile.Write("crtsurfdata2 开始运行。\n");
 
   for(int ii=0;ii<560000000;ii++){
 
 	  logfile.Write("这是第%010d条日志\n",ii+"abcd");
   }
-  logfile.Write("crtsurfdata1 运行结束.   \n");
+  logfile.Write("crtsurfdata2 运行结束.   \n");
 
 
   return 0; 
